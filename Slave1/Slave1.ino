02037e506ba84c6746241f75ea6610f1b0146f33
Jung Jun Hyeok <ted0505@naver.com>
2022-05-15 15:55:48 +0900

    slave + display

--- Slave1/Slave1.ino
+++ Slave1/Slave1.ino
@@ -2,10 +2,25 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include <TFT_eSPI.h>
+#include <SPI.h>
+#include "Button2.h"
+#include "esp_adc_cal.h"
+#include "bmp.h"
 
 #define rxPin 25
 #define txPin 26
 #define SWITCH1 32
+
+// 디스플레이를 통해 전압확인과 동작을 위한 핀 선언
+#define ADC_EN              14  //ADC_EN is the ADC detection enable port
+#define ADC_PIN             34
+#define BUTTON_1            35
+#define BUTTON_2            0
+
+TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
+Button2 btn1(BUTTON_1);
+Button2 btn2(BUTTON_2);
 
 long baud = 9600;
 
@@ -15,6 +30,62 @@ Adafruit_MPU6050 mpu;
 sensors_event_t a, g, temp;
 void printAvailableData();
 void serialPrintAvailableData();
+
+void espDelay(int ms)
+{
+    esp_sleep_enable_timer_wakeup(ms * 1000);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
+    esp_light_sleep_start();
+}
+
+void showVoltage()
+{
+    static uint64_t timeStamp = 0;
+    if (millis() - timeStamp > 1000) {
+        timeStamp = millis();
+        uint16_t v = analogRead(ADC_PIN);
+        float battery_voltage = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
+        String voltage = "Voltage :" + String(battery_voltage) + "V";
+        Serial.println(voltage);
+        tft.fillScreen(TFT_BLACK);
+        tft.setTextDatum(MC_DATUM);
+        tft.drawString(voltage,  tft.width() / 2, tft.height() / 2 );
+    }
+}
+
+void button_init()
+{
+    btn1.setPressedHandler([](Button2 & b) {
+        Serial.println("Detect Voltage..");
+        btnCick = true;
+    });
+    btn2.setPressedHandler([](Button2 & b) {
+        btnCick = false;
+        int r = digitalRead(TFT_BL);
+        tft.fillScreen(TFT_BLACK);
+        tft.setTextColor(TFT_GREEN, TFT_BLACK);
+        tft.setTextDatum(MC_DATUM);
+        tft.drawString("Press again to wake up",  tft.width() / 2, tft.height() / 2 );
+        espDelay(6000);
+        digitalWrite(TFT_BL, !r);
+
+        tft.writecommand(TFT_DISPOFF);
+        tft.writecommand(TFT_SLPIN);
+        //After using light sleep, you need to disable timer wake, because here use external IO port to wake up
+        esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_TIMER);
+        // esp_sleep_enable_ext1_wakeup(GPIO_SEL_35, ESP_EXT1_WAKEUP_ALL_LOW);
+        esp_sleep_enable_ext0_wakeup(GPIO_NUM_35, 0);
+        delay(200);
+        esp_deep_sleep_start();
+    });
+}
+
+void button_loop()
+{
+    btn1.loop();
+    btn2.loop();
+}
+
  
 void setup() {
   pinMode(rxPin, INPUT);
@@ -22,6 +93,24 @@ void setup() {
   pinMode(SWITCH1, INPUT_PULLUP);
   Serial.begin(baud); 
   HC12.begin(baud, SERIAL_8N1, rxPin, txPin);             // 시리얼 모니터 속도 정의
+
+  pinMode(ADC_EN, OUTPUT);
+  digitalWrite(ADC_EN, HIGH);
+
+  tft.init();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextSize(2);
+  tft.setTextColor(TFT_GREEN);
+  tft.setCursor(0, 0);
+  tft.setTextDatum(MC_DATUM);
+  tft.setTextSize(1);
+
+  tft.setSwapBytes(true);
+  tft.pushImage(0, 0,  240, 135, ttgo);
+  espDelay(5000);
+
+  button_init();
   
   // MPU6050 센서 I2C 통신 테스트
   if (!mpu.begin()) {
@@ -96,6 +185,11 @@ void setup() {
 }
  
 void loop() {
+  if (btnCick) {
+        showVoltage();
+    }
+    button_loop();
+  
   if(digitalRead(SWITCH1) == HIGH) { 
     mpu.enableSleep(true);
     Serial.println("SWITCH OFF"); 
