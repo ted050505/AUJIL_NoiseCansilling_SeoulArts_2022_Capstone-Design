d9af2339918e734fdb0b99a46586e9b3a5649846
Jung Jun Hyeok <ted0505@naver.com>
2022-06-07 12:54:08 +0900

    ㅁㅇㅇ

--- Slave_test/Slave_test.ino
+++ Slave_test/Slave_test.ino
@@ -18,18 +18,18 @@ void setup() {
 }
 
 void loop() {
-//    while (HC12.available()) {        // HC-12에 수신 데이터가 존재하는 경우
-//    Serial.write(HC12.read());      // HC-12 모듈의 출력 내용을 읽어 시리얼 모니터로 전송
-//  }
-//  while (Serial.available()) {
-////    sender();                         // 시리얼 모니터의 입력 내용이 존재하면
-//    HC12.write(Serial.read());      // 읽어서 HC-12 모듈로 전송
-//  }
-  if(digitalRead(SWITCH1) == HIGH) { 
-    Serial.println("SWITCH OFF"); 
-  }else{
-    Serial.println("SWITCH ON"); 
+    while (HC12.available()) {        // HC-12에 수신 데이터가 존재하는 경우
+    Serial.write(HC12.read());      // HC-12 모듈의 출력 내용을 읽어 시리얼 모니터로 전송
   }
+  while (Serial.available()) {
+//    sender();                         // 시리얼 모니터의 입력 내용이 존재하면
+    HC12.write(Serial.read());      // 읽어서 HC-12 모듈로 전송
+  }
+//  if(digitalRead(SWITCH1) == HIGH) { 
+//    Serial.println("SWITCH OFF"); 
+//  }else{
+//    Serial.println("SWITCH ON"); 
+//  }
 }
 
 void sender(void) {
