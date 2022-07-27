ed114d10ba324a7c818587b2cd40c9f0aa4a1a71
Jung Jun Hyeok <ted0505@naver.com>
2022-07-27 16:59:37 +0900

    add stand light code

--- Master1/Master1.ino
+++ Master1/Master1.ino
@@ -15,16 +15,11 @@ void setup() {
   pinMode(rxPin, INPUT);
 //      pinMode(txPin, OUTPUT);
   Serial.begin(9600);             // 시리얼 모니터 속도 정의
-//    HC12.begin(9600);               // HC12 시리얼 통신 속도 정의
-
-  //   HC12 모듈 통신 테스트
-//      if ,
 }
 
 void loop() {
   while (Serial.available()) {        // HC-12에 수신 데이터가 존재하는 경우
     Serial.write(Serial.read());      // HC-12 모듈의 출력 내용을 읽어 시리얼 모니터로 전송
-    //    Serial.print(HC12.read());
   }
 //    while (Serial.available()) {      // 시리얼 모니터의 입력 내용이 존재하면
 //      HC12.write(Serial.read());      // 읽어서 HC-12 모듈로 전송
---
+++ Stand_light/Stand_light.ino
@@ -0,0 +1,105 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+#define PIN        6
+#define NUMPIXELS 3 // Popular NeoPixel ring size
+
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+#define DELAYVAL 500
+
+int previousMillis = 0;
+int interval = 5;
+
+void setup() {
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  Serial.begin(9600);
+  pixels.begin();
+  pixels.clear();
+  pixels.setBrightness(255);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  while (Serial.available() > 0) {
+    char c = Serial.read();
+    Serial.print(c);
+
+    // 헤드폰 미 착용 시 동작
+    if (c == '1') {
+      pixels.clear();
+      for (int u = 0; u < 255; u++) {
+        for (int i = 0; i < 3; i++) {
+          if (currentMillis - previousMillis > interval) {
+            previousMillis = currentMillis;
+
+            pixels.setBrightness(u);
+            pixels.setPixelColor(i, pixels.Color(255, 130, 30));
+            pixels.show();   // Send the updated pixel colors to the hardware.
+          }
+        }
+      }
+    }
+    // 헤드폰 착용 시 동작
+    if (c == '2') {
+      pixels.clear();
+      for (int u = 0; u < 255; u++) {
+        for (int i = 0; i < 3; i++) {
+          if (currentMillis - previousMillis > interval) {
+            previousMillis = currentMillis;
+            
+            pixels.setBrightness(u);
+            pixels.setPixelColor(i, pixels.Color(50, 255, 50));
+            pixels.show();   // Send the updated pixel colors to the hardware.
+          }
+        }
+      }
+    }
+    // 헤드폰 4명 착용 시 동작
+    if (c == '3') {
+      pixels.clear();
+      for (int u = 0; u < 255; u++) {
+        for (int i = 0; i < 3; i작++) {
+          if (currentMillis - previousMillis > interval) {
+            previousMillis = currentMillis;
+            
+            pixels.setBrightness(u);
+            pixels.setPixelColor(i, pixels.Color(255, 20, 20));
+            pixels.show();   // Send the updated pixel colors to the hardware.
+          }
+        }
+      }
+    }
+  }
+}
+
+
+//if (currentMillis - previousMillis > interval) {
+//  previousMillis = currentMillis;
+//
+//  if (c == '1') {
+//    pixels.clear();
+//    for (int u = 0; u < 255; u++) {
+//      for (int i = 0; i < 3; i++) {
+//        pixels.setBrightness(u);
+//        pixels.setPixelColor(i, pixels.Color(255, 130, 30));
+//        pixels.show();   // Send the updated pixel colors to the hardware.
+//      }
+//    }
+//  }
+//  if (c == '2') {
+//    pixels.clear();
+//    for (int u = 0; u < 255; u++) {
+//      for (int i = 0; i < 3; i++) {
+//        pixels.setBrightness(u);
+//        pixels.setPixelColor(i, pixels.Color(50, 255, 50));
+//        pixels.show();   // Send the updated pixel colors to the hardware.
+//      }
+//    }
+//  }
+//}
